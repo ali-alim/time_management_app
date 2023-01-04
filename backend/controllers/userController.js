0f36cfcbf8fab49074b826d457befae7c926ba07
ali-alim <ali.mamedov.2016@gmail.com>
2023-01-04 22:47:02 +0400

    create route and controller function for changing password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -168,7 +168,7 @@ const updateUser = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    const {name, email, photo, phone, bio } = user;
+    const { name, email, photo, phone, bio } = user;
 
     user.email = email;
     user.name = req.body.name || name;
@@ -177,6 +177,7 @@ const updateUser = asyncHandler(async (req, res) => {
     user.bio = req.body.bio || bio;
 
     const updatedUser = await user.save();
+
     res.status(200).json({
       _id: updatedUser._id,
       name: updatedUser.name,
@@ -187,8 +188,40 @@ const updateUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(404);
-    throw new Error("No user was found")
+    throw new Error("No user was found");
   }
+});
+
+// Change password
+const changePassword = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  const {oldPassword, password} = req.body;
+
+  if(!user){
+    res.status(400)
+    throw new Error("User not found, please sign up")
+
+  }
+  // Validate
+  if(!oldPassword || !password){
+    res.status(400)
+    throw new Error("Please add old and new passwords")
+  }
+
+  // Check if old password is correct
+  const passwordIsCorrect = await bcrypt.compare(oldPassword, user.password);
+
+  // Save new password
+  if(user && passwordIsCorrect){
+    user.password = password;
+    await user.save();
+    res.status(200).send("Password was changed successfully")
+  } else {
+    res.status(400);
+    throw new Error("Old password is incorrect")
+  }
+  
 });
 
 module.exports = {
@@ -198,4 +231,5 @@ module.exports = {
   getUser,
   loginStatus,
   updateUser,
+  changePassword,
 };
